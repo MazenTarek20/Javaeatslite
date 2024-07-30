246054cf26422ec59cbde0631dfa1d83bbcc4c0d
MazenTarek20 <114245709+MazenTarek20@users.noreply.github.com>
2024-07-30 17:10:07 +0300

    Add files via upload

---
+++ src/test/java/com/example/Javaeats/JavaeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Javaeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaeatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
